ae061d603d71718c8bbf3bc91e472e92c7ef43a2
Kim Jeong Hun <jeonhun6084@gmail.com>
2023-04-17 11:18:00 +0900

    default

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     $("#banner").hide();
+    $("#gallery").hide();
     
     $(".nextPage").on("click", function(){
         $("#intro").hide(); 
@@ -9,8 +10,55 @@ $( document ).ready(function() {
     });
 
     $(".hex-cell").on("click", function(){
-        $("#banner").fadeOut(600); 
+        $("#banner").fadeOut(600,function(){
+            $("link[title='title1']").attr("href","assets/css/test.css");
+            $("#gallery").fadeIn(600);
+        });
         return;
     });
+
+    $("#btn_back").on("click", function(){
+        $("#gallery").fadeOut(600,function(){
+            $("link[title='title1']").attr("href","assets/css/main.css");
+            $("#banner").fadeIn(600);
+        });
+    });
+
+    $('.gallery-link').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+          verticalFit: true,
+          titleSrc: function(item) {
+            return item.el.find('figcaption').text() || item.el.attr('title');
+          }
+        },
+        zoom: {
+          enabled: true
+        },
+        // duration: 300
+        gallery: {
+          enabled: true,
+          navigateByImgClick: false,
+          tCounter: ''
+        },
+        disableOn: function() {
+          return $(window).width() > 640;
+        }
+    });
+    let img_list = ["./assets/img/aa.gif"]
+    let img_text = ["1111"]
+    String.prototype.format = function() {
+      var formatted = this;
+      for( var arg in arguments ) {
+          formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+      }
+      return formatted;
+    };
+    $.each(img_list, function(index, item){
+      $(".gallery").text("<a class='gallery-link' href='./assets/img/aa.gif'><figure class='gallery-image'><img height='1600' src='./assets/img/aa.gif' width='1600'><figcaption>1111</figcaption></figure></a>").format;
+    })
     
 });
