f99b53d8d650f5e84c14140ce539778f497f42a6
jeonhun6084 <jeonhun6084@gmail.com>
2023-05-04 23:19:18 +0900

    default

---
+++ assets/js/main.js
@@ -0,0 +1,185 @@
+(function ($) {
+	let $window = $(window),
+		$body = $('body');
+
+	$('#main2').hide();
+
+	// Breakpoints.
+	breakpoints({
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: ['361px', '480px'],
+		xxsmall: [null, '360px'],
+	});
+
+	// Play initial animations on page load.
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
+
+	// Touch?
+	if (browser.mobile) $body.addClass('is-touch');
+
+	// Forms.
+	let $form = $('form');
+
+	// Auto-resizing textareas.
+	$form.find('textarea').each(function () {
+		let $this = $(this),
+			$wrapper = $('<div class="textarea-wrapper"></div>'),
+			$submits = $this.find('input[type="submit"]');
+
+		$this
+			.wrap($wrapper)
+			.attr('rows', 1)
+			.css('overflow', 'hidden')
+			.css('resize', 'none')
+			.on('keydown', function (event) {
+				if (event.keyCode == 13 && event.ctrlKey) {
+					event.preventDefault();
+					event.stopPropagation();
+
+					$(this).blur();
+				}
+			})
+			.on('blur focus', function () {
+				$this.val($.trim($this.val()));
+			})
+			.on('input blur focus --init', function () {
+				$wrapper.css('height', $this.height());
+
+				$this.css('height', 'auto').css('height', $this.prop('scrollHeight') + 'px');
+			})
+			.on('keyup', function (event) {
+				if (event.keyCode == 9) $this.select();
+			})
+			.triggerHandler('--init');
+
+		// Fix.
+		if (browser.name == 'ie' || browser.mobile) $this.css('max-height', '10em').css('overflow-y', 'auto');
+	});
+
+	// Menu.
+	let $menu = $('#menu');
+
+	$menu.wrapInner('<div class="inner"></div>');
+
+	$menu._locked = false;
+
+	$menu._lock = function () {
+		if ($menu._locked) return false;
+
+		$menu._locked = true;
+
+		window.setTimeout(function () {
+			$menu._locked = false;
+		}, 350);
+
+		return true;
+	};
+
+	$menu._show = function () {
+		if ($menu._lock()) $body.addClass('is-menu-visible');
+	};
+
+	$menu._hide = function () {
+		if ($menu._lock()) $body.removeClass('is-menu-visible');
+	};
+
+	$menu._toggle = function () {
+		if ($menu._lock()) $body.toggleClass('is-menu-visible');
+	};
+
+	$menu
+		.appendTo($body)
+		.on('click', function (event) {
+			event.stopPropagation();
+		})
+		.on('click', 'a', function (event) {
+			let href = $(this).attr('href');
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			// Hide.
+			$menu._hide();
+
+			// Redirect.
+			if (href == '#menu') return;
+
+			window.setTimeout(function () {
+				window.location.href = href;
+			}, 350);
+		})
+		.append('<a class="close" href="#menu">Close</a>');
+
+	let $modal = $('.modal');
+
+	$modal.on('hidden.bs.modal', function (e) {
+		$('iframe')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[1].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[2].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[3].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[4].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[5].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+	});
+
+	let $nav = $('nav');
+
+	$nav.on('click', function (event) {
+		$('iframe')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[1].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[2].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[3].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[4].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+		$('iframe')[5].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+	});
+
+	$body
+		.on('click', 'a[href="#menu"]', function (event) {
+			event.stopPropagation();
+			event.preventDefault();
+
+			// Toggle.
+			$menu._toggle();
+		})
+		.on('click', function (event) {
+			// Hide.
+			$menu._hide();
+		})
+		.on('keydown', function (event) {
+			// Hide on escape.
+			if (event.keyCode == 27) $menu._hide();
+		});
+
+	let $darkBtn = $('#darkBtn');
+	$darkBtn.on('click', function () {
+		if ($darkBtn.hasClass('fas fa-toggle-on')) {
+			toggleLight();
+		} else {
+			toggleDark();
+		}
+	});
+
+	// Dark Mode Functions
+	function toggleDark() {
+		// Change button classes
+		$darkBtn.attr('class', 'fas fa-toggle-on');
+		$('#main').hide();
+		$('#main2').show();
+	}
+
+	// Light Mode Functions
+	function toggleLight() {
+		// Change button classes
+		$darkBtn.attr('class', 'fas fa-toggle-off');
+		$('#main').show();
+		$('#main2').hide();
+
+		// Change button disabled boolean
+	}
+})(jQuery);
